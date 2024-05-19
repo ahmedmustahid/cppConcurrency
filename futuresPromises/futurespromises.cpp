2c9e10b5777054c7c639a5adafa381da45b66044
ahmedmustahid <ahmed@epx.phys.tohoku.ac.jp>
2024-05-19 16:50:23 +0900

    using shared_future for 2 threads

--- futuresPromises/futurespromises.cpp
+++ futuresPromises/futurespromises.cpp
@@ -11,7 +11,8 @@ void produce(std::promise<int> &px)
     px.set_value(x);
 }
 
-void consume(std::future<int> &fut)
+// void consume(std::future<int> &fut)
+void consume(std::shared_future<int> &fut)
 {
     int x = fut.get();
     cout << "received the value " << x << "\n";
@@ -20,10 +21,12 @@ void consume(std::future<int> &fut)
 int main()
 {
     std::promise<int> prom;
-    std::future fut = prom.get_future();
+    // std::future fut = prom.get_future();
+    std::shared_future<int> fut = prom.get_future();
+    std::shared_future<int> fut2 = fut;
     std::thread t1(produce, std::ref(prom));
     std::thread t2(consume, std::ref(fut));
-    std::thread t3(consume, std::ref(fut));
+    std::thread t3(consume, std::ref(fut2));
 
     t1.join();
     t2.join();
