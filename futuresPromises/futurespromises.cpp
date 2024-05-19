5976bcd903e0a85f2323179486f4231aaae0bc4b
ahmedmustahid <ahmed@epx.phys.tohoku.ac.jp>
2024-05-19 16:21:48 +0900

    futuresPromises

---
+++ futuresPromises/futurespromises.cpp
@@ -0,0 +1,29 @@
+#include <functional>
+#include <future>
+#include <iostream>
+#include <thread>
+
+using std::cout;
+
+void produce(std::promise<int> &px)
+{
+    int x = 45;
+    px.set_value(x);
+}
+
+void consume(std::future<int> &fut)
+{
+    int x = fut.get();
+    cout << "received the value " << x << "\n";
+}
+
+int main()
+{
+    std::promise<int> prom;
+    std::future fut = prom.get_future();
+    std::thread t1(produce, std::ref(prom));
+    std::thread t2(consume, std::ref(fut));
+
+    t1.join();
+    t2.join();
+}
